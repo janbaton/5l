bf84a724c860c259a710c98d087aab1d7d71fade
janbaton <apacho.38@gmail.com>
2016-06-17 22:46:04 -0700

    ~

--- src/sqr.c
+++ src/sqr.c
@@ -1,6 +1,6 @@
 #include "sqr.h"
 
-	int sqrquat(float a, b, d, *x1, *x2);
+	int sqrquat(float a, float b, float d, float *x1, float *x2)
 {
 	if (d>0)
 	{
@@ -8,11 +8,14 @@
 		*x2 = (-b - sqrt(d)) / (2 * a);
 		return FOUND_2_ROOTS;
 	}
+
 	if (d == 0)
+
 	{
 		*x1 = -b / (2 * a);
 		return FOUND_1_ROOT;
 	}
+
 	if (d < 0)
-		return ROOTS_NOT_FOUND;
+		return ROOT_NOT_FOUND;
 }
--- test/sqr_test.c
+++ test/sqr_test.c
@@ -5,34 +5,34 @@ CTEST(sqrquat_suite, two_roots)
 {
 	//Given
 	const float a = 1;
-	const float b = 4;
-	const float c = 3;
+	const float b = -13;
+	const float c = 25;
 
 	//When
 	float x1, x2;
 	const int result = sqrquat(a, b, c, &x1, &x2);
 
 	//Then
-	const float expected_x1 = -1, expected_x2 = -3;
+	const float expected_x1 = 9, expected_x2 = 4;
 
 	ASSERT_DBL_NEAR(expected_x1, x1);
 	ASSERT_DBL_NEAR(expected_x2, x2);
-	ASSERT_EQUAL(FOUNT_2_ROOTS, result);
+	ASSERT_EQUAL(FOUND_2_ROOTS, result);
 }
 
 CTEST(sqrquat_suite, one_root)
 {
 	//Given
-	const float a = 1;
-	const float b = 2;
-	const float c = 1;
+	const float a = 5;
+	const float b = -20;
+	const float c = 0;
 
 	//When
 	float x1, x2;
 	const int result = sqrquat(a, b, c, &x1, &x2);
 
 	//Then
-	const float  expected_x1 = -1;
+	const float  expected_x1 = 2;
 
 	ASSERT_DBL_NEAR(expected_x1, x1);
 	ASSERT_EQUAL(FOUND_1_ROOT, result);
@@ -41,14 +41,14 @@ CTEST(sqrquat_suite, one_root)
 CTEST(sqrquat_suite, no_root)
 {
 	//Given
-	const float a = 1;
-	const float b = 2;
-	const float c = 2;
+	const float a = 3;
+	const float b = -12;
+	const float c = -15;
 
 	//When
 	float x1, x2;
 	const int result = sqrquat(a, b, c, &x1, &x2);
 
 	//Then
-	ASSERT_EQUAL(ROOTS_NOT_FOUND, result);
+	ASSERT_EQUAL(ROOT_NOT_FOUND, result);
 }
